cf74eec720b2fc9c9cf9965feb362ec8b6598424
Han-Bin Song <thdgksq@naver.com>
2023-01-20 10:10:51 +0900

    프로젝트 세팅

---
+++ Member/src/main/java/bean/MemberBean.java
@@ -0,0 +1,96 @@
+package bean;
+
+public class MemberBean {
+
+	private String id;
+	private String pwd;
+	private String name;
+	private String gender;
+	private String email;
+	private String birth;
+	private String zipcode;
+	private String address;
+	private String hobby;
+	private String job;
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+}
--- Member/src/main/java/ttt/Wwww.java
+++
@@ -1,7 +0,0 @@
-package ttt;
-
-public class Wwww {
-public Wwww() {
-	// TODO Auto-generated constructor stub
-}
-}
