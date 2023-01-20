a005058df59f3d7e0d60e5478fc3ea0c802d80db
Han-Bin Song <thdgksq@naver.com>
2023-01-20 16:02:17 +0900

    js파일 복구

---
+++ Member/src/main/webapp/script.js
@@ -0,0 +1,28 @@
+function zipserch(){ 
+	if(document.frm.zip.value==null){ 
+		alert("주소를 입력해주세요"); 
+		return false;	 
+	}else if(document.frm.zip.value==""){ 
+		alert("주소를 입력해주세요"); 
+		return false;	 
+	} 
+	return true; 
+} 
+function checkId(){ 
+	if(document.idCheckForm.chid.value==null){ 
+		alert("ID를 입력해주세요."); 
+		return false; 
+	} 
+	if(checkNum==1){ 
+		alert("ID가 중복됩니다."); 
+		return false; 
+	} 
+	else{ 
+		return true; 
+	} 
+} 
+ 
+function goUrl() { 
+	const idText = document.f.id.value; 
+	return "idCheck.jsp?id="+idText; 
+} 
